40c547c08b05b755dabf0347dbc7787635916562
Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2022-12-07 17:23:11 +0200

    Finalised colors, style and everything else for category cell

--- app/src/main/java/com/example/youthero/categories/Category.kt
+++ app/src/main/java/com/example/youthero/categories/Category.kt
@@ -6,11 +6,15 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -27,27 +31,36 @@ data class Category(val categoryTitle: String, @DrawableRes val categoryImageID:
 //rounded corners
 
 @Composable
-fun CategoryItem(categoryItem: Category) {
-    //Cell == Big column
+fun CategoryItem(categoryItem: Category) {//Cell == Big column
     Column(
         modifier = Modifier
-            .padding(start = 12.dp, top = 12.dp, end = 12.dp, bottom = 14.dp)
-    ) {
+            .padding(all = 16.dp)
+            .clip(shape = RoundedCornerShape(10.dp))
+            .background(Color.White)
+            .shadow(
+                elevation = 6.dp,
+                shape = RectangleShape,
+                clip = true,
+                ambientColor = Color.White,
+                spotColor = Color(0x001E1E1E)
+            )
+    ){
         Image(
             painter = painterResource(categoryItem.categoryImageID),
             contentDescription = null,
             modifier = Modifier
+                .padding(start = 12.dp, top = 12.dp, end = 12.dp)
                 .size(137.dp, 143.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = categoryItem.categoryTitle,
             modifier = Modifier
-                .wrapContentWidth(),
+                .padding(start = 12.dp, bottom = 14.dp),
             color = Color(0xFF6A6A6A), //dirty gray
             fontStyle = FontStyle(0), //normal
             fontWeight = FontWeight(600), //weight
-            fontFamily = FontFamily.SansSerif,
+            fontFamily = FontFamily.SansSerif, //SP nush
             lineHeight = 16.sp
         )
     }
@@ -56,6 +69,7 @@ fun CategoryItem(categoryItem: Category) {
 @Composable
 fun MenuCategories(listCategories: List<Category>) {
     LazyColumn(modifier = Modifier
+        .background(Color(0x001E1E1E))
         //.verticalScroll(rememberScrollState())
     ) {
         listCategories.map { item { CategoryItem(it) } }
@@ -73,14 +87,14 @@ fun MenuCategoriesGrid(listCategories: List<Category>) {
 @Composable
 fun Preview() {
     val listCategories: List<Category> = listOf(
-        Category("Educatie", R.drawable.educatie),/*
+        Category("Educatie", R.drawable.educatie),
         Category("Cultura", R.drawable.cultura),
         Category("Ocupatie", R.drawable.ocupatie),
         Category("Mediu inconjurator", R.drawable.mediu_inconjurator),
         Category("Viata sanatoasa", R.drawable.viata_sanatoasa),
         Category("Drepturile Tinerilor", R.drawable.drepturi_tineri),
         Category("Participare", R.drawable.participare),
-        Category("Spatii pentru tineri", R.drawable.spatii_tineri)*/
+        Category("Spatii pentru tineri", R.drawable.spatii_tineri)
     )
     YoutheroTheme {
         MenuCategories(listCategories)
