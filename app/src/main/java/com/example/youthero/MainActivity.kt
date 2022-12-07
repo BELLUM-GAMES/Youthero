05a1717c229c7af85686b366e9b5dfe99eecc4ef
Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2022-12-07 16:55:15 +0200

    More Text properties added

--- app/src/main/java/com/example/youthero/MainActivity.kt
+++ app/src/main/java/com/example/youthero/MainActivity.kt
@@ -12,28 +12,45 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.youthero.categories.Category
+import com.example.youthero.categories.MenuCategories
 import com.example.youthero.ui.theme.YoutheroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val listCategories: List<Category> = listOf(
+                Category("Educatie", R.drawable.educatie),
+                Category("Cultura", R.drawable.cultura),
+                Category("Ocupatie", R.drawable.ocupatie),
+                Category("Mediu inconjurator", R.drawable.mediu_inconjurator),
+                Category("Viata sanatoasa", R.drawable.viata_sanatoasa),
+                Category("Drepturile Tinerilor", R.drawable.drepturi_tineri),
+                Category("Participare", R.drawable.participare),
+                Category("Spatii pentru tineri", R.drawable.spatii_tineri)
+            )
             YoutheroTheme {
-                //Actual app and not preview
+                MenuCategories(listCategories)
             }
         }
     }
 }
 
 @Composable
-fun MenuItem(itemTitle: String, @DrawableRes itemImageID: Int){
+fun MenuItem(itemDescription: String, @DrawableRes itemImage: Int){
     Column {
         Image(
-            painter = painterResource(itemImageID),
+            painter = painterResource(itemImage),
             contentDescription = null,
+            /*
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)*/
         )
         Column {
-            Text(itemTitle)
+            Text(itemDescription)
         }
     }
 }
@@ -43,34 +60,34 @@ fun MenuWithoutLazy() {
     Column {
         Row{
             Column {
-                MenuItem(itemTitle = "Educatie", itemImageID = R.drawable.educatie)
+                MenuItem(itemDescription = "Educatie", itemImage = R.drawable.educatie)
             }
             Column {
-                MenuItem(itemTitle = "Cultura", itemImageID = R.drawable.cultura)
-            }
-        }
-        Row{
-            Column {
-                MenuItem(itemTitle = "Ocupatie", itemImageID = R.drawable.ocupatie)
-            }
-            Column {
-                MenuItem(itemTitle = "Mediu inconjurator", itemImageID = R.drawable.mediu_inconjurator)
+                MenuItem(itemDescription = "Cultura", itemImage = R.drawable.cultura)
             }
         }
         Row{
             Column {
-                MenuItem(itemTitle = "Viata sanatoasa", itemImageID = R.drawable.viata_sanatoasa)
+                MenuItem(itemDescription = "Ocupatie", itemImage = R.drawable.ocupatie)
             }
             Column {
-                MenuItem(itemTitle = "Drepturile Tinerilor", itemImageID = R.drawable.drepturi_tineri)
+                MenuItem(itemDescription = "Mediu inconjurator", itemImage = R.drawable.mediu_inconjurator)
             }
         }
         Row{
             Column {
-                MenuItem(itemTitle = "Participare", itemImageID = R.drawable.participare)
+                MenuItem(itemDescription = "Viata sanatoasa", itemImage = R.drawable.viata_sanatoasa)
             }
             Column {
-                MenuItem(itemTitle = "Spatii pentru tineri", itemImageID = R.drawable.spatii_tineri)
+                MenuItem(itemDescription = "Drepturile Tinerilor", itemImage = R.drawable.drepturi_tineri)
+            }
+        }
+        Row{
+            Column {
+                MenuItem(itemDescription = "Participare", itemImage = R.drawable.participare)
+            }
+            Column {
+                MenuItem(itemDescription = "Spatii pentru tineri", itemImage = R.drawable.spatii_tineri)
             }
         }
     }
--- app/src/main/java/com/example/youthero/categories/Category.kt
+++ app/src/main/java/com/example/youthero/categories/Category.kt
@@ -2,72 +2,85 @@ package com.example.youthero.categories
 
 import android.content.res.Configuration
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.youthero.MenuItem
+import androidx.compose.ui.unit.*
 import com.example.youthero.R
 import com.example.youthero.ui.theme.YoutheroTheme
 
 data class Category(val categoryTitle: String, @DrawableRes val categoryImageID: Int)
 
+//pt txt wrap content
+//scroll state apartine lazycolumn
+//rounded corners
+
 @Composable
 fun CategoryItem(categoryItem: Category) {
-    Column {
+    //Cell == Big column
+    Column(
+        modifier = Modifier
+            .padding(start = 12.dp, top = 12.dp, end = 12.dp, bottom = 14.dp)
+    ) {
         Image(
             painter = painterResource(categoryItem.categoryImageID),
             contentDescription = null,
+            modifier = Modifier
+                .size(137.dp, 143.dp)
         )
-        Column {
-            Text(categoryItem.categoryTitle)
-        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = categoryItem.categoryTitle,
+            modifier = Modifier
+                .wrapContentWidth(),
+            color = Color(0xFF6A6A6A), //dirty gray
+            fontStyle = FontStyle(0), //normal
+            fontWeight = FontWeight(600), //weight
+            fontFamily = FontFamily.SansSerif,
+            lineHeight = 16.sp
+        )
     }
 }
 
 @Composable
 fun MenuCategories(listCategories: List<Category>) {
-    LazyColumn{
-        listCategories.map { item { CategoryItem(it)} }
+    LazyColumn(modifier = Modifier
+        //.verticalScroll(rememberScrollState())
+    ) {
+        listCategories.map { item { CategoryItem(it) } }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun LazyVerticalGridDemo(listCategories: List<Category>){
+fun MenuCategoriesGrid(listCategories: List<Category>) {
+    //LazyVerticalGrid(cells = , content = )
 }
 
 @Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
 )
 @Composable
 fun Preview() {
     val listCategories: List<Category> = listOf(
-        Category("Educatie", R.drawable.educatie),
+        Category("Educatie", R.drawable.educatie),/*
         Category("Cultura", R.drawable.cultura),
         Category("Ocupatie", R.drawable.ocupatie),
         Category("Mediu inconjurator", R.drawable.mediu_inconjurator),
         Category("Viata sanatoasa", R.drawable.viata_sanatoasa),
         Category("Drepturile Tinerilor", R.drawable.drepturi_tineri),
         Category("Participare", R.drawable.participare),
-        Category("Spatii pentru tineri", R.drawable.spatii_tineri)
+        Category("Spatii pentru tineri", R.drawable.spatii_tineri)*/
     )
     YoutheroTheme {
         MenuCategories(listCategories)
