958d27bce45c726da0dad3f0e1394727e15304bc
Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2022-12-07 11:56:46 +0200

    Made Menu function without lazy elements that generate the basic menu without modifieres

--- app/src/main/java/com/example/youthero/MainActivity.kt
+++ app/src/main/java/com/example/youthero/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.youthero
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
@@ -24,28 +27,67 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MenuItem(itemDescription: String, @DrawableRes itemImage: Int){
-    Image(
-        painter = painterResource(itemImage),
-        contentDescription = null,
-        /*
-        modifier = Modifier
-            .size(40.dp)
-            .clip(CircleShape)
-            .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)*/
-    )
-    Text(itemDescription)
+    Column {
+        Image(
+            painter = painterResource(itemImage),
+            contentDescription = null,
+            /*
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)*/
+        )
+        Column {
+            Text(itemDescription)
+        }
+    }
 }
 
 @Composable
-fun Menu() {
-    MenuItem(itemDescription = "Educatie", itemImage = R.drawable.educatie)
+fun MenuWithoutLazy() {
+    Column {
+        Row{
+            Column {
+                MenuItem(itemDescription = "Educatie", itemImage = R.drawable.educatie)
+            }
+            Column {
+                MenuItem(itemDescription = "Cultura", itemImage = R.drawable.cultura)
+            }
+        }
+        Row{
+            Column {
+                MenuItem(itemDescription = "Ocupatie", itemImage = R.drawable.ocupatie)
+            }
+            Column {
+                MenuItem(itemDescription = "Mediu inconjurator", itemImage = R.drawable.mediu_inconjurator)
+            }
+        }
+        Row{
+            Column {
+                MenuItem(itemDescription = "Viata sanatoasa", itemImage = R.drawable.viata_sanatoasa)
+            }
+            Column {
+                MenuItem(itemDescription = "Drepturile Tinerilor", itemImage = R.drawable.drepturi_tineri)
+            }
+        }
+        Row{
+            Column {
+                MenuItem(itemDescription = "Participare", itemImage = R.drawable.participare)
+            }
+            Column {
+                MenuItem(itemDescription = "Spatii pentru tineri", itemImage = R.drawable.spatii_tineri)
+            }
+        }
+    }
 }
 
-@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode")
 @Composable
 fun Preview()
 {
     YoutheroTheme {
-        Menu()
+        MenuWithoutLazy()
     }
 }
