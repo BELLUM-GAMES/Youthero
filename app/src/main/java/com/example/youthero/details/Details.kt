a65c2124625b9217594205577aa68b7584b499b9
Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2022-12-08 13:37:00 +0200

    Deleted unused imports

--- app/src/main/java/com/example/youthero/details/Details.kt
+++ app/src/main/java/com/example/youthero/details/Details.kt
@@ -4,14 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import com.example.youthero.ui.theme.YoutheroTheme
 
 @Composable
 fun Details(navController: NavController) {
     YoutheroTheme {
-        Column{
+        Column {
             Button(onClick = {
                 navController.navigate("categories")
             }) {
