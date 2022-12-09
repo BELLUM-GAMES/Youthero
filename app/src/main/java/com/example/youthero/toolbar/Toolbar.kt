ed83608f6e3d7c7db19d09b1e83ab9f03d856f3d
Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2022-12-09 13:19:04 +0200

    Init commit

---
+++ app/src/main/java/com/example/youthero/toolbar/Toolbar.kt
@@ -0,0 +1,51 @@
+package com.example.youthero.toolbar
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.youthero.R
+import com.example.youthero.categories.Category
+import com.example.youthero.categories.MenuCategories
+import com.example.youthero.ui.theme.YoutheroTheme
+
+@Composable
+fun Details(categoryTitle: String?) {
+    YoutheroTheme {
+        Column {
+            Text("This is $categoryTitle layout")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    val listCategories: List<Category> = listOf(
+        Category("Educatie", R.drawable.educatie),
+    )
+    YoutheroTheme {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "categories") {
+            composable("categories") {
+                MenuCategories(
+                    listCategories = listCategories,
+                    navController = navController
+                )
+            }
+            composable(
+                "details/{categoryTitle}",
+                arguments = listOf(navArgument("categoryTitle") {
+                    type = NavType.StringType
+                })
+            ) {
+                com.example.youthero.details.Details(categoryTitle = it.arguments?.getString("categoryTitle"))
+            }
+        }
+    }
+}
